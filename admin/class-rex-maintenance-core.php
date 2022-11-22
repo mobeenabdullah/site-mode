618f61140cf139f372f21b15b7c22e11c989d795
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-11-22 11:44:25 -0800

    seo andadvanced fields

---
+++ admin/class-rex-maintenance-core.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Responsible for plugin menu
+ *
+ * @link       https://https://mobeenabdullah.com
+ * @since      1.0.0
+ *
+ * @package    Rex_Maintenance_Mode
+ * @subpackage Rex_Maintenance_Mode/includes
+ */
+
+/**
+ * Responsible for plugin menu
+ *
+ * This class defines all code necessary to run during the plugin's menu
+ *
+ * @since      1.0.0
+ * @package    Rex_Maintenance_Mode
+ * @subpackage Rex_Maintenance_Mode/includes
+ * @author     Mobeen Abdullah <mobeenabdullah@gmail.com>
+ */
+class Rex_Maintenance_Core {
+
+    public function __construct()
+    {
+
+    }
+
+    public function wp_header_files() {
+
+    }
+
+}
