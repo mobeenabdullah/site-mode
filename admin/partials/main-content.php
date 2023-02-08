c58f208b5423197c0979403a4b92bcfb0787cfa4
Mobeen Abdullah <mobeenabdullah@gmail.com>
2023-02-09 02:01:10 +0500

    Tabs UI styling and sidebar

--- admin/partials/main-content.php
+++ admin/partials/main-content.php
@@ -60,7 +60,7 @@
             foreach ($site_mode_tabs as $tab) :
                 $tab_class = strtolower($tab['title']) === $active_tab ? 'sm_tabs-link current' : 'sm_tabs-link';
                 $tab_data = 'tab-' . strtolower($tab['title']);
-                echo '<li class="' . esc_attr($tab_class) . '" data-tab="' . esc_attr($tab_data) . '">' . $this->wp_kses_svg($tab['icon']) . esc_html($tab['title'], 'site-mode') . '</li>';
+                echo '<li class="' . esc_attr($tab_class) . '" data-tab="' . esc_attr($tab_data) . '"><span class="menu_icon">' . $this->wp_kses_svg($tab['icon']) . '</span><span class="menu_label">' . esc_html($tab['title'], 'site-mode') . '</span></li>';
             endforeach; ?>
     </ul>
 
