fffe526338f78ae32ec01912378dda32bde8e507
Mobeen Abdullah <mobeenabdullah@gmail.com>
2023-02-09 23:15:08 +0500

    Overall UI Styling

--- admin/partials/sidebar.php
+++ admin/partials/sidebar.php
@@ -1,10 +1,10 @@
 
-<div class="preview_box">
-    <div class="preview_box_header">
-        <h3><?php _e('Currently Active Theme','site-mode');?></h3>
+<div class="sidebar_card">
+    <div class="sidebar_card_header">
+        <h3 class="card_title"><?php _e('Active Theme','site-mode');?></h3>
     </div>
-    <div class="preview_box_body">
-        <div class="preview_box_body_content">
+    <div class="sidebar_card_body">
+        <div class="sidebar_card_body_content">
             <div class="preview_active_template" style="background-image: url('<?php echo plugin_dir_url( __DIR__ ) .'assets/img/template-1.jpg'; ?>)">    
                 <div class="preview_active_template_link">        
                     <a href="<?php echo esc_url(home_url( '?site-mode-preview=true')); ?>" class="btn btn_sm btn_white" target="_blank">
@@ -12,6 +12,17 @@
                     </a>
                 </div>    
             </div>
+        </div>       
+    </div>    
+</div>
+
+<div class="sidebar_card">
+    <div class="sidebar_card_header">
+        <h3 class="card_title"><?php _e('About Plugin','site-mode');?></h3>
+    </div>
+    <div class="sidebar_card_body">
+        <div class="sidebar_card_body_content">
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, odit minima soluta dolorum possimus magnam est eos fuga obcaecati fugiat, vitae molestiae aut nesciunt nihil iste architecto dicta nobis consequatur!</p>
         </div>       
     </div>    
 </div>
