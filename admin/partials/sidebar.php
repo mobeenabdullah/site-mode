6d9c81ff8954a9b33dab9cd5ab084f13d4dad67a
Mobeen Abdullah <mobeenabdullah@gmail.com>
2023-02-15 23:32:27 +0500

    Feedback fixes

--- admin/partials/sidebar.php
+++ admin/partials/sidebar.php
@@ -4,10 +4,8 @@
         <h3 class="card_title"><?php _e('Preview','site-mode');?></h3>
     </div>
     <div class="sidebar_card_body">
-        <div class="sidebar_card_body_content">
-            <div class="submit_button">
-                <a href="<?php echo esc_url(home_url( '?site-mode-preview=true')); ?>" target="_blank" class="preview_btn"><?php _e('Preview Active Template','site-mode');?></a>
-            </div>
+        <div class="sidebar_card_body_content">            
+                <a href="<?php echo esc_url(home_url( '?site-mode-preview=true')); ?>" target="_blank"><?php _e('Preview Active Template','site-mode');?> <span class="external_link"><i class="fa-solid fa-arrow-up-right-from-square"></i></span></a>            
         </div>       
     </div>    
 </div>
--- public/templates/default_template.php
+++ public/templates/default_template.php
@@ -14,6 +14,7 @@ $design_social              = unserialize(get_option('site_mode_design_social'))
 $social                     = unserialize(get_option('site_mode_social'));
 $seo                        = unserialize(get_option('site_mode_seo'));
 $general                    = unserialize(get_option('site_mode_general'));
+$show_login_icon            = isset($general['show_login_icon']) ? $general['show_login_icon'] : '';
 $custom_login_url           = isset($general['custom_login_url']) ? $general['custom_login_url'] : '';
 $logo_url                   = isset($content['logo_image']) ? wp_get_attachment_image_url($content['logo_image'], 'full') : '';
 $image_url                  = isset($content['bg_image']) ? wp_get_attachment_image_url($content['bg_image'], 'full') : '';
@@ -82,17 +83,20 @@ require_once 'footer.php';
     <section id="under_constructon" class="wrapper" style="background-image: url('<?php echo  esc_url($image_url) ?>')">
         <!--Section Overlay-->
         <div class="wrapper_overlay"></div>
-            <div class="login_icon">
-                <?php if(empty($custom_login_url)) {?>
-                <a href="<?php  echo esc_url(get_home_url()) . '/wp-login.php' ?>" style="display: block">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="40"  height="40" viewBox="0 0 24 24" style="fill:#ffffff"><path d="M12 2C9.243 2 7 4.243 7 7v3H6c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-8c0-1.103-.897-2-2-2h-1V7c0-2.757-2.243-5-5-5zm6 10 .002 8H6v-8h12zm-9-2V7c0-1.654 1.346-3 3-3s3 1.346 3 3v3H9z" fill="#ffffff"></path></svg>
-                </a>
-                <?php } else {  ?>
-                    <a href="<?php echo  esc_url(get_home_url()) .'/'.$custom_login_url  ?>" style="display: block">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="40"  height="40" viewBox="0 0 24 24" style="fill:#ffffff"><path d="M12 2C9.243 2 7 4.243 7 7v3H6c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-8c0-1.103-.897-2-2-2h-1V7c0-2.757-2.243-5-5-5zm6 10 .002 8H6v-8h12zm-9-2V7c0-1.654 1.346-3 3-3s3 1.346 3 3v3H9z" fill="#ffffff"></path></svg>
-                </a>
-                <?php }?>
-            </div>
+            <?php if($show_login_icon) : ?>
+                <div class="login_icon">
+                    <?php if(empty($custom_login_url)) : ?>
+                    <a href="<?php  echo esc_url(get_home_url()) . '/wp-login.php' ?>" style="display: block">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40"  height="40" viewBox="0 0 24 24" style="fill:#ffffff"><path d="M12 2C9.243 2 7 4.243 7 7v3H6c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-8c0-1.103-.897-2-2-2h-1V7c0-2.757-2.243-5-5-5zm6 10 .002 8H6v-8h12zm-9-2V7c0-1.654 1.346-3 3-3s3 1.346 3 3v3H9z" fill="#ffffff"></path></svg>
+                    </a>
+                    <?php else :  ?>
+                        <a href="<?php echo  esc_url(get_home_url()) .'/'.$custom_login_url  ?>" style="display: block">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="40"  height="40" viewBox="0 0 24 24" style="fill:#ffffff"><path d="M12 2C9.243 2 7 4.243 7 7v3H6c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-8c0-1.103-.897-2-2-2h-1V7c0-2.757-2.243-5-5-5zm6 10 .002 8H6v-8h12zm-9-2V7c0-1.654 1.346-3 3-3s3 1.346 3 3v3H9z" fill="#ffffff"></path></svg>
+                        </a>
+                    <?php endif; ?>
+                </div>
+            <?php else : ?>
+            <?php endif; ?>
         <!--Section Content-->
         <div class="container">
             <div class="default_template">
