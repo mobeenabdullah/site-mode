1e8dc46d1742ea98b9b755ff7ae85337076a11cd
Mobeen Abdullah <mobeenabdullah@gmail.com>
2023-08-31 18:51:17 +0500

    Template-activation-functionlity

--- admin/partials/settings-layout.php
+++ admin/partials/settings-layout.php
@@ -2,6 +2,13 @@
 	<div class="site_mode__wrap--header">		
 		<img src="<?php echo esc_url( plugins_url( '/assets/img/site-mode-logo.svg', dirname( __FILE__ ) ) ); ?>" alt="Site Mode Logo" class="site_mode__wrap--logo">
 	</div>
+
+    <?php
+    if(empty(get_option('sm-fresh-installation'))):
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'partials/template-initialization.php';
+    else :
+    ?>
+
 	<div class="site_mode__wrap--cover">
 		<div class="site_mode__wrap--cover-content">
 			<?php require_once plugin_dir_path( dirname( __FILE__ ) ) . 'partials/main-content.php'; ?>
@@ -10,4 +17,6 @@
 			<?php require_once plugin_dir_path( dirname( __FILE__ ) ) . 'partials/sidebar.php'; ?>
 		</div>
 	</div>
+
+    <?php endif; ?>
 </div>
