27ccd26addef93e8b751a785f847eba334917c5a
Mobeen Abdullah <mobeenabdullah@gmail.com>
2023-10-04 23:21:58 +0500

    Fix countdown block css issues

--- blocks/site-mode-countdown/build/index.asset.php
+++ blocks/site-mode-countdown/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '58e7fe0c9b687555ae67');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '38e486ed607d778f56f2');
