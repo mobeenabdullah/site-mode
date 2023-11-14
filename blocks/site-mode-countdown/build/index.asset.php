73fec4fff0b28574fd7f9065b1182c9f6667ef66
Mobeen Abdullah <mobeenabdullah@gmail.com>
2023-11-14 19:35:48 +0500

    Countdown responsive

--- blocks/site-mode-countdown/build/index.asset.php
+++ blocks/site-mode-countdown/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bf3a1f0ea20eaa076573');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9903c123ecd2bd37ac14');
